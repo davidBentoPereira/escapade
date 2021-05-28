4cf6e3f661c86aab87f944c6047a2c3c4c755b33
David BENTO-PEREIRA <oobento01oo@gmail.com>
2021-05-29 00:27:01 +0200

    feat: add bootstrap

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,16 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# frozen_string_literal: true
+
+# Remove all data
+Post.destroy_all
+
+# Create Posts
+10.times do |i|
+  Post.create!(
+    title: "Title #{i}",
+    content: "This is a description for the titre #{i}"
+  )
+end
+
+puts "All posts were created !"
+
+
