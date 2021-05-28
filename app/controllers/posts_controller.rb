1b5a0e8b2cf0da76c21862c54d9014b99e88e15b
David BENTO-PEREIRA <oobento01oo@gmail.com>
2021-05-28 22:32:08 +0200

    feat: add front post form

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+class PostsController < ApplicationController
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+
+    respond_to do |format|
+      if @post.save
+        format.html { redirect_to root_path, notice: 'Post was successfully created.' }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+      end
+    end
+  end
+end
+
+private
+
+# Only allow a list of trusted parameters through.
+def post_params
+  params.require(:post).permit(:title, :content)
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
 
   root to: 'homepage#index'
-
+  resources :posts
 
   namespace :admin do
     resources :posts
